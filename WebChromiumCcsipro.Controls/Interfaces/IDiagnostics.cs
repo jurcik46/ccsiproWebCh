34d5a3833c328ca4eb7a2f5b1a7e53aae9e203db
jurcik46 <jurcik46@gmail.com>
2019-01-29 19:44:01 +0100

    Diagnostic class

---
+++ WebChromiumCcsipro.Controls/Interfaces/IDiagnostics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebChromiumCcsipro.Controls.Interfaces
+{
+    public interface IDiagnostics
+    {
+        string CommandLine { get; }
+        string Version { get; }
+        string VersionDeploy { get; }
+        bool IsNetworkDeployed { get; }
+        string CurrentDirectory { get; }
+        string UserDomainName { get; }
+        string MachineName { get; }
+        string UserName { get; }
+        OperatingSystem OSVersion { get; }
+        string SystemDirectory { get; }
+        bool Is64BitOperatingSystem { get; }
+        bool Is64BitProcess { get; }
+        int ProcessorCount { get; }
+        TimeSpan UpTime { get; }
+        string CLRVersion { get; }
+        string WorkingSet { get; }
+        string StackTrace { get; }
+    }
+}
