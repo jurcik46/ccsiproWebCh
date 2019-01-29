06016681db682b4d2d6e20c3ce3962bbe4c53087
jurcik46 <jurcik46@gmail.com>
2019-01-29 19:43:37 +0100

    main windows view

--- WebChromiumCcsipro.UI/Views/MainWindow/MainWindowView.xaml.cs
+++ WebChromiumCcsipro.UI/Views/MainWindow/MainWindowView.xaml.cs
@@ -11,17 +11,33 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using WebChromiumCcsipro.UI.ViewModels;
 
 namespace WebChromiumCcsipro.UI.Views.MainWindow
 {
     /// <summary>
     /// Interaction logic for MainWindowView.xaml
     /// </summary>
+    /// [STAThread]
     public partial class MainWindowView : Window
     {
         public MainWindowView()
         {
             InitializeComponent();
+            this.DataContext = ViewModelLocator.MainViewModel;
+        }
+
+        private void MainWindowView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var splashScreen = ViewModelLocator.SplashScreen;
+            if (splashScreen == null)
+            {
+                return;
+            }
+            splashScreen.Close(TimeSpan.FromMilliseconds(200));
+            ViewModelLocator.SplashScreen = null;
+
         }
     }
 }
