5323bb5f376683d0d48b8fef6ec4dd37fd47fe99
jurcik46 <jurcik46@gmail.com>
2019-02-10 16:25:04 +0100

    init setting view model

---
+++ WebChromiumCcsipro.UI/ViewModels/SettingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using WebChromiumCcsipro.Controls.Interfaces;
+
+namespace WebChromiumCcsipro.UI.ViewModels
+{
+    public class SettingViewModel : ViewModelBase
+    {
+
+        public SettingViewModel()
+        {
+
+        }
+    }
+}
