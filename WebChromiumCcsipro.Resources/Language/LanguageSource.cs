06473a1a0eeeaad0556de7784acfc0de92c06bca
jurcik46 <jurcik46@gmail.com>
2019-02-10 16:24:15 +0100

    List of languages

---
+++ WebChromiumCcsipro.Resources/Language/LanguageSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WebChromiumCcsipro.Resources.Language
+{
+    public class LanguageSource
+    {
+
+        static public Dictionary<string, string> GetValues()
+        {
+            var languages = new Dictionary<string, string>() { { "en-US", "English" }, { "cs-CZ", "Čeština" }, { "sk-SK", "Slovenčina" } };
+            return languages;
+        }
+    }
+}
