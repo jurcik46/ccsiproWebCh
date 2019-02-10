ef3edbaea43cc64546fa32c02bec753327ffbebf
jurcik46 <jurcik46@gmail.com>
2019-02-10 16:41:58 +0100

    create folder for setting view models

---
+++ WebChromiumCcsipro.UI/ViewModels/SettingViewModel/SettingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace WebChromiumCcsipro.UI.ViewModels.SettingViewModel
+{
+    public class SettingViewModel : ViewModelBase
+    {
+
+        public SettingViewModel()
+        {
+
+        }
+    }
+}
