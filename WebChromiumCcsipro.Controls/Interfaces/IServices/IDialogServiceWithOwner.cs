65494c89954e1ddf3682d67cdc8912186ac6452b
jurcik46 <jurcik46@gmail.com>
2019-02-10 17:15:29 +0100

    Interface for dialog service

---
+++ WebChromiumCcsipro.Controls/Interfaces/IServices/IDialogServiceWithOwner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WebChromiumCcsipro.Controls.Interfaces.IServices
+{
+    public interface IDialogServiceWithOwner : GalaSoft.MvvmLight.Views.IDialogService
+    {
+        bool HideAllErrors { get; set; }
+        Window Owner { get; set; }
+        string ChangePassword();
+        Task ShowError(string message, string title, string buttonText, Action afterHideCallback, bool alwaysShow);
+        Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText, Action<bool> afterHideCallback, bool defaultCancel);
+        Task ShowWarning(string message, string title);
+
+
+    }
+}
