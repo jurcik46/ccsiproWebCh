1583fde4df0e002e659c20635d2e881e5c1243a4
jurcik46 <jurcik46@gmail.com>
2019-02-12 20:55:10 +0100

    crypto extension

---
+++ WebChromiumCcsipro.Controls/Extensions/CryptoExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebChromiumCcsipro.Controls.Extensions
+{
+    public static class CryptoExtension
+    {
+        public static string GenerateSalt()
+        {
+            var bytes = new byte[64];
+            var csprng = new RNGCryptoServiceProvider();
+            csprng.GetBytes(bytes);
+            var salt64 = Convert.ToBase64String(bytes);
+            return salt64;
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+            var text = salt + password;
+            var bytes = Encoding.UTF8.GetBytes(text);
+            using (var sha = new SHA512CryptoServiceProvider())
+            {
+                var hash = sha.ComputeHash(bytes);
+                var hash64 = Convert.ToBase64String(hash);
+                return hash64;
+            }
+        }
+
+        public static bool VerifyPassword(string password, string salt, string hashedPassword)
+        {
+            var newHash = HashPassword(password, salt);
+            var result = newHash.Equals(hashedPassword, StringComparison.Ordinal);
+            return result;
+        }
+    }
+}
