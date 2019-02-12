2efdd2c21a6fc673792739d1f90db04769e0e285
jurcik46 <jurcik46@gmail.com>
2019-02-12 18:21:00 +0100

    setting service setup signature setting

--- WebChromiumCcsipro.Controls/Interfaces/IServices/ISettingsService.cs
+++ WebChromiumCcsipro.Controls/Interfaces/IServices/ISettingsService.cs
@@ -8,7 +8,19 @@ namespace WebChromiumCcsipro.Controls.Interfaces.IServices
 {
     public interface ISettingsService
     {
-        void LoadSetting();
+        #region Signature setting
+        string ApiLink { get; set; }
+        string ApiKey { get; set; }
+        string ProgramPath { get; set; }
+        string ProcessName { get; set; }
+        int SignatureTimeOut { get; set; }
+        #endregion
+
+        void LoadAllSetting();
         void SaveSetting();
+        void SignatureSettingLoad();
+
+        void SignatureSettingSave(string apiLink, string apiKey, string programPath, string processName,
+            int signatureTimeOut = 100);
     }
 }
--- WebChromiumCcsipro.Controls/Services/SettingsService.cs
+++ WebChromiumCcsipro.Controls/Services/SettingsService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Destructurama.Attributed;
 using Serilog;
+using WebChromiumCcsipro.Resources.Settings;
 using WebChromiumCcsipro.Controls.Interfaces.IServices;
 
 namespace WebChromiumCcsipro.Controls.Services
@@ -12,14 +13,37 @@ namespace WebChromiumCcsipro.Controls.Services
     [LogAsScalar]
     public class SettingsService : ISettingsService
     {
-        public ILogger Logger => Log.Logger.ForContext<SettingsService>();
+        private ILogger Logger => Log.Logger.ForContext<SettingsService>();
+
+        #region Signature setting
+        public string ApiLink { get; set; }
+        public string ApiKey { get; set; }
+        public string ProgramPath { get; set; }
+        public string ProcessName { get; set; }
+        public int SignatureTimeOut { get; set; }
+        #endregion
 
         public SettingsService()
         {
-            LoadSetting();
+            LoadAllSetting();
         }
 
-        public void LoadSetting()
+        public void LoadAllSetting()
+        {
+            ChromiumSettingLoad();
+            SignatureSettingLoad();
+        }
+
+        public void SignatureSettingLoad()
+        {
+            ApiLink = SignatureSetting.Default.ApiLink;
+            ApiKey = SignatureSetting.Default.ApiKey;
+            ProgramPath = SignatureSetting.Default.ProgramPath;
+            ProcessName = SignatureSetting.Default.ProcessName;
+            SignatureTimeOut = SignatureSetting.Default.singTimeOut;
+        }
+
+        private void ChromiumSettingLoad()
         {
 
         }
@@ -28,6 +52,17 @@ namespace WebChromiumCcsipro.Controls.Services
         {
 
         }
+
+        public void SignatureSettingSave(string apiLink, string apiKey, string programPath, string processName, int signatureTimeOut = 100)
+        {
+            SignatureSetting.Default.ApiLink = apiLink;
+            SignatureSetting.Default.ApiKey = apiKey;
+            SignatureSetting.Default.ProgramPath = programPath;
+            SignatureSetting.Default.ProcessName = processName;
+            SignatureSetting.Default.singTimeOut = signatureTimeOut;
+            SignatureSetting.Default.Save();
+        }
+
 
 
 
