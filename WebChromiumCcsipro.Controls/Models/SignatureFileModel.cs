d83db56c39fcc78cf1fa86b7f0bbc9b897926690
jurcik46 <jurcik46@gmail.com>
2019-02-13 15:17:04 +0100

    signature file model for service

---
+++ WebChromiumCcsipro.Controls/Models/SignatureFileModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebChromiumCcsipro.Resources.Interfaces;
+using WebChromiumCcsipro.Resources.Interfaces.IServices;
+
+namespace WebChromiumCcsipro.Controls.Models
+{
+    public class SignatureFileModel : ISignatureFileModel
+    {
+
+        public string PdfFilePath { get; set; }
+        public string Hash { get; set; }
+        public byte[] File { get; set; }
+
+        public SignatureFileModel(API.Models.SignatureFileModel signatueFile)
+        {
+            PdfFilePath = signatueFile.PdfFilePath;
+            Hash = signatueFile.Hash;
+            File = Convert.FromBase64String(signatueFile.File);
+        }
+    }
+}
