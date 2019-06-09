24b4398000e0370ce773c9d8071ecbe04826eaf8
Ján Jurč <jurcik46@gmail.com>
2019-06-09 20:26:08 +0200

    motion detecet socket model

---
+++ WebChromiumCcsipro.BusinessLogic/Models/MotionDetectSocketModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Script.Serialization;
+
+namespace WebChromiumCcsipro.BusinessLogic.Models
+{
+    public class MotionDetectSocketModel
+    {
+        public int ObjectId { get; set; }
+        public string VideoDeviceSource { get; set; }
+        public string ZoneName { get; set; }
+        [ScriptIgnore]
+        public DateTime TimeMotion { get; set; }
+
+        private string _time;
+        public string Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                TimeMotion = DateTime.ParseExact(value, "dd.MM.yyyy HH:mm:ss",
+                    System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
