2c61f4831fcc4535b67d3ff4ba3e8ef81e0ef1a2
Ján Jurč <jurcik46@gmail.com>
2019-06-09 20:28:41 +0200

    delete unused atribute in notifi mesage

--- WebChromiumCcsipro.Domain/Messages/NotifiMessage.cs
+++ WebChromiumCcsipro.Domain/Messages/NotifiMessage.cs
@@ -11,10 +11,9 @@ namespace WebChromiumCcsipro.Domain.Messages
         private NotificationType _iconType;
         private int _expTime = 5;
 
-        public string Title { get => _title; set => _title = AppName + value; }
+        public string Title { get => _title; set => _title = _appName + value; }
         public string Msg { get => _msg; set => _msg = value; }
         public NotificationType IconType { get => _iconType; set => _iconType = value; }
         public int ExpTime { get => _expTime; set => _expTime = value; }
-        public string AppName { get => _appName; set => _appName = value; }
     }
 }
