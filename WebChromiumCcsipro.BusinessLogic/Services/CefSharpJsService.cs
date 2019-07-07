f6a295c2b80cf4c4acbb69ccd3f3d0be25a368bd
Ján Jurč <jurcik46@gmail.com>
2019-07-07 20:38:00 +0200

    sendcamre link method

--- WebChromiumCcsipro.BusinessLogic/Services/CefSharpJsService.cs
+++ WebChromiumCcsipro.BusinessLogic/Services/CefSharpJsService.cs
@@ -1,29 +1,44 @@
 ﻿using System.Threading.Tasks;
+using Serilog;
+using WebChromiumCcsipro.Domain.Enums;
+using WebChromiumCcsipro.Domain.Extensions;
 using WebChromiumCcsipro.Domain.Interfaces.IServices;
 
 namespace WebChromiumCcsipro.BusinessLogic.Services
 {
     public class CefSharpJsService : ICefSharpJsService
     {
-        private ISignatureService SignatureService { get; set; }
+        public ILogger Logger => Log.Logger.ForContext<CefSharpJsService>();
+
+        private ISignatureService _signatureService { get; set; }
+        private ISocketService _socketService { get; set; }
 
 
-        public CefSharpJsService(ISignatureService signatureService)
+        public CefSharpJsService(ISignatureService signatureService, ISocketService socketService)
         {
-            SignatureService = signatureService;
+            Logger.Information(CefSharpJsServiceEvents.Create, "Creating new instance of CefSharpJsService");
+            _signatureService = signatureService;
+            _socketService = socketService;
         }
 
 
         public void bozpSignatureJsAsync()
         {
-            if (SignatureService.InProcces)
+            if (_signatureService.InProcces)
                 return;
+            Logger.Information(CefSharpJsServiceEvents.Create);
 
             Task.Run(() =>
             {
-                SignatureService.StartSign();
+                _signatureService.StartSign();
 
             });
+        }
+
+        public void sendCameralink(string link)
+        {
+            Logger.Information(CefSharpJsServiceEvents.sendCameralink, $"Camera Link {link}");
+            _socketService.KioskSendData(link);
         }
 
 
